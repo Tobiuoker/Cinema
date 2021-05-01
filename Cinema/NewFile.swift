f81b4d0ba0dd986822a506b4adcae41ff0fc0f37
Khaled Gamri <gamri.kh@rahmetapp.kz>
2021-05-01 16:50:00 +0600

    temp1

--- Cinema/NewFile.swift
+++ Cinema/NewFile.swift
@@ -9,3 +9,12 @@
 import Foundation
 
 
+final class NewFIle {
+    var temp1: String
+    var temp2: Int
+    
+    init(temp1: String, temp2: Int) {
+        self.temp1 = temp1
+        self.temp2 = temp2
+    }
+}
